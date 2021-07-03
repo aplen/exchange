f385f7228b1f45b886a9c2e8c27a85bd026a2514
Adam Plencler <aplencler@gmail.com>
2021-07-03 18:07:57 +0200

    Fix null error

--- src/main/java/aplencler/exchange/ExchangeRates.java
+++ src/main/java/aplencler/exchange/ExchangeRates.java
@@ -14,9 +14,15 @@ public class ExchangeRates {
 
     public HashMap<String, Double> findExchangeRates(String currencyCode) {
 
+        final LinkedHashMap<String, Double> DEFAULT_EXCHANGE_RATES_MAP = new LinkedHashMap() {
+            {
+                put("bid", 0.00);
+                put("ask", 0.00);
+            }};
+
         HashMap<String, ArrayList<LinkedHashMap<String, Double>>> exchangeRatesMap;
         ObjectMapper mapper = new ObjectMapper();
-        LinkedHashMap<String, Double> exchangeRatesList = null;
+        LinkedHashMap<String, Double> exchangeRatesList;
 
         try {
             URL url = new URL("http://api.nbp.pl/api/exchangerates/rates/C/" + currencyCode + "/?format=json");
@@ -25,6 +31,7 @@ public class ExchangeRates {
 
         } catch (IOException e) {
             System.err.println("Exchange rate retrieve fail");
+            exchangeRatesList = DEFAULT_EXCHANGE_RATES_MAP;
 
         }
         return exchangeRatesList;
