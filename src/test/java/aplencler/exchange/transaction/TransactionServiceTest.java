8a5af8f10d98486981748664ab1aae5b28d83502
Adam Plencler <aplencler@gmail.com>
2021-07-03 17:56:12 +0200

    remove unnecessary Spring context

--- src/test/java/aplencler/exchange/transaction/TransactionServiceTest.java
+++ src/test/java/aplencler/exchange/transaction/TransactionServiceTest.java
@@ -1,47 +1,43 @@
 package aplencler.exchange.transaction;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.LinkedHashMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
 public class TransactionServiceTest {
 
 
-    @Autowired
-    TransactionService transactionService;
-    LinkedHashMap<String, Double> mockExchangeRatesMap;
+    TransactionService transactionService = new TransactionService();
+    LinkedHashMap<String, Double> mockExchangeRatesMap = new LinkedHashMap() {
+        {
+            put("bid", 10.00);
+            put("ask", 11.00);
+        }
+    };
 
-    @Before
-    public void initialize() {
-        mockExchangeRatesMap = new LinkedHashMap<>();
-        mockExchangeRatesMap.put("bid", 10.00);
-        mockExchangeRatesMap.put("ask", 11.00);
 
-    }
+
 
     @Test
-    public void shouldReturnCorrectValueWhenAsk() throws Exception {
+    public void shouldReturnCorrectValueWhenAsk()  {
         assertThat(transactionService.countValue(10.0, "ask", mockExchangeRatesMap)).isEqualTo(112.20);
     }
+
     @Test
-    public void shouldReturnCorrectValueWhenBid() throws Exception {
+    public void shouldReturnCorrectValueWhenBid()  {
         assertThat(transactionService.countValue(10.0, "bid", mockExchangeRatesMap)).isEqualTo(98.00);
     }
+
     @Test
-    public void shouldReturnZeroWhenAskZeroAmount() throws Exception {
+    public void shouldReturnZeroWhenAskZeroAmount() {
         assertThat(transactionService.countValue(0.0, "ask", mockExchangeRatesMap)).isEqualTo(0.00);
     }
+
     @Test
-    public void shouldReturnZeroWhenBidZeroAmount() throws Exception {
+    public void shouldReturnZeroWhenBidZeroAmount() {
         assertThat(transactionService.countValue(0.0, "bid", mockExchangeRatesMap)).isEqualTo(0.00);
     }
 
