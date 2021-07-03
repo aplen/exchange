d6e34140e2f34339f3cf652a8c1a3c538be4de2a
Adam Plencler <aplencler@gmail.com>
2021-07-03 17:55:23 +0200

    Update ExchangeRates.java

--- src/main/java/aplencler/exchange/ExchangeRates.java
+++ src/main/java/aplencler/exchange/ExchangeRates.java
@@ -6,24 +6,26 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 @Component
 public class ExchangeRates {
 
-    public LinkedHashMap<String, Double> findExchangeRates(String currencyCode) {
+    public HashMap<String, Double> findExchangeRates(String currencyCode) {
 
-        LinkedHashMap<String, ArrayList<LinkedHashMap<String, Double>>> exchangeRatesMap;
+        HashMap<String, ArrayList<LinkedHashMap<String, Double>>> exchangeRatesMap;
         ObjectMapper mapper = new ObjectMapper();
         LinkedHashMap<String, Double> exchangeRatesList = null;
 
         try {
             URL url = new URL("http://api.nbp.pl/api/exchangerates/rates/C/" + currencyCode + "/?format=json");
-            exchangeRatesMap = mapper.readValue(url, LinkedHashMap.class);
+            exchangeRatesMap = mapper.readValue(url, HashMap.class);
             exchangeRatesList = exchangeRatesMap.get("rates").get(0);
 
         } catch (IOException e) {
             System.err.println("Exchange rate retrieve fail");
+
         }
         return exchangeRatesList;
     }
